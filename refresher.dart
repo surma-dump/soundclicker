b6556c10711ba7892eb154a2ae6e0db9ef4c54f8
Alexander Surma <surma@surmair.de>
2012-04-23 00:47:25 +0200

    Implement proper reloading

--- refresher.dart
+++ refresher.dart
@@ -1,15 +1,38 @@
 #import ("dart:html");
 
 String template = @'
-<div style="position: fixed; bottom: 0; right: 0" id="soundclicker">
+<div style="position: fixed; bottom: 0; right: 0; background-color: white; border: 2px solid black;" id="soundclicker">
 	Start SoundClicker: <button>Go</button>
 </div>';
+
+void injectHTML(String html) {
+	var ctrl_panel = new Element.html(html);
+	document.query("body").nodes.add(ctrl_panel);
+}
+
+void reloadPage() {
+	var url = window.location.href;
+	if(!isActive()) {
+		url += "&active";
+	}
+	window.location.href = url;
+}
+
+bool isActive() {
+	var url = window.location.href;
+	return url.contains("&active");
+}
+
 void main() {
-	// var ctrl_panel = new Element(template);
-	// document.query("body").nodes.add(ctrl_panel);
+	injectHTML(template);
+	document.query("#soundclicker button").on.click.add((event) {
+		print("in here");
+		reloadPage();
+	});
+
 	var t = document.query("#myform table tr:last-child td:not(.label)");
-	if(t.innerHTML.contains("Sorry")) {
-		//window.location.href = window.location.href;
-		print("Would reload");
+	print("Active? "+isActive());
+	if(t.innerHTML.contains("Sorry") && isActive()) {
+		reloadPage();
 	}
 }
